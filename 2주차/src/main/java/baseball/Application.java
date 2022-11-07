9dfd1d7ee41e95b11839fad1ae3bad5f7a1f5f20
dmdrk1414 <seungchan141414@gmail.com>
2022-11-07 21:07:23 +0900

    [Add] java 소스 테스트 코드 추가

---
+++ 2주차/src/main/java/baseball/Application.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import view.GameView;
+
+public class Application {
+
+    public static void main(String[] args) {
+        GameView gameView = new GameView();
+        gameView.run();
+    }
+}
